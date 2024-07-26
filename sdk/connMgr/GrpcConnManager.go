ddf511a26cd9fa9e64b4733ad58bcb48930a06be
dongzesu@tencent.com <riyuejiuzhao@qq.com>
2024-07-26 22:13:37 +0800

    更新sdk连接管理

--- sdk/connMgr/GrpcConnManager.go
+++ sdk/connMgr/GrpcConnManager.go
@@ -1,121 +1,87 @@
 package connMgr
 
 import (
-	"Supernove2024/sdk"
+	"Supernove2024/sdk/config"
+	"Supernove2024/sdk/util"
+	"errors"
 	"github.com/shimingyah/pool"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/backoff"
-	"google.golang.org/grpc/credentials/insecure"
-	"google.golang.org/grpc/keepalive"
-	"time"
 )
+
+type ServiceType int32
 
 const (
-	// DialTimeout the timeout of create connection
-	DialTimeout = 5 * time.Second
+	Discovery ServiceType = iota
+	HealthCheck
+	Register
 
-	// KeepAliveTime is the duration of time after which if the client doesn't see
-	// any activity it pings the server to see if the transport is still alive.
-	KeepAliveTime = time.Duration(10) * time.Second
-
-	// KeepAliveTimeout is the duration of time for which the client waits after having
-	// pinged for keepalive check and if no activity is seen even after that the connection
-	// is closed.
-	KeepAliveTimeout = time.Duration(3) * time.Second
-
-	// InitialWindowSize we set it 1GB is to provide system's throughput.
-	InitialWindowSize = 1 << 30
-
-	// InitialConnWindowSize we set it 1GB is to provide system's throughput.
-	InitialConnWindowSize = 1 << 30
-
-	// MaxSendMsgSize set max gRPC request message size sent to server.
-	// If any request message size is larger than current value, an error will be reported from gRPC.
-	MaxSendMsgSize = 4 << 30
-
-	// MaxRecvMsgSize set max gRPC receive message size received from server.
-	// If any message size is larger than current value, an error will be reported from gRPC.
-	MaxRecvMsgSize = 4 << 30
-
-	// RetryPolicy 重传配置
-	RetryPolicy = `{
-        "methodConfig": [{
-            "name": [
-				{"service": "RegisterService"},
-				{"service": "DiscoveryService"}
-			],
-            "retryPolicy": {
-                "MaxAttempts": 4,
-                "InitialBackoff": "0.1s",
-                "MaxBackoff": "1s",
-                "BackoffMultiplier": 2,
-                "RetryableStatusCodes": [ "UNAVAILABLE" ]
-            }
-        }]
-    }`
+	ServiceTypeCount
 )
 
-var DefaultOptions = pool.Options{
-	Dial:                 Dial,
-	MaxIdle:              8,
-	MaxActive:            64,
-	MaxConcurrentStreams: 64,
-	Reuse:                true,
-}
-
-func Dial(address string) (*grpc.ClientConn, error) {
-	return grpc.NewClient(address, grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithConnectParams(grpc.ConnectParams{
-			Backoff: backoff.Config{
-				BaseDelay:  1 * time.Second,
-				Multiplier: 1.6,
-				Jitter:     0.2,
-				MaxDelay:   120 * time.Second},
-			MinConnectTimeout: DialTimeout}),
-		grpc.WithInitialWindowSize(InitialWindowSize),
-		grpc.WithInitialConnWindowSize(InitialConnWindowSize),
-		grpc.WithDefaultCallOptions(grpc.MaxCallSendMsgSize(MaxSendMsgSize)),
-		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(MaxRecvMsgSize)),
-		grpc.WithKeepaliveParams(keepalive.ClientParameters{
-			Time:                KeepAliveTime,
-			Timeout:             KeepAliveTimeout,
-			PermitWithoutStream: true,
-		}),
-		grpc.WithDefaultServiceConfig(RetryPolicy))
-}
+type AddressPoolDic map[string]pool.Pool
 
 // GrpcConnManager 管理GRPC链接
 type GrpcConnManager struct {
-	poolDic map[string]pool.Pool
+	config  *config.Config
+	poolDic map[ServiceType]AddressPoolDic
 }
 
-func ConnManagerInstance()
+var connMgr *GrpcConnManager = nil
 
-func newConnManager(services []sdk.ServiceConfig) *GrpcConnManager {
+func Instance() (*GrpcConnManager, error) {
+	if connMgr == nil {
+		cfg, err := config.GlobalConfig()
+		if err != nil {
+			return nil, errors.New("创建连接管理器失败")
+		}
+		connMgr = newConnManager(cfg.Global.RegisterService, cfg.Global.DiscoverService)
+	}
+	return connMgr, nil
+}
+
+func newAddressPoolDic(serviceConfig []config.ServiceConfig) AddressPoolDic {
 	poolDic := make(map[string]pool.Pool)
-	for _, service := range services {
+	for _, service := range serviceConfig {
 		address := service.String()
 		p, err := pool.New(service.String(), DefaultOptions)
 		if err != nil {
-			sdk.Error("创建连接池失败，地址：%s", address)
+			util.Error("创建连接池失败，地址：%s", address)
 			continue
 		}
-		sdk.Info("创建连接池，地址: %s", address)
+		util.Info("创建连接池，地址: %s", address)
 		poolDic[address] = p
 	}
-	sdk.Info("初始化GrpcConnManager成功")
+	return poolDic
+}
+
+func newConnManager(register []config.ServiceConfig,
+	discovery []config.ServiceConfig) *GrpcConnManager {
+	poolDic := make(map[ServiceType]AddressPoolDic)
+	poolDic[Discovery] = newAddressPoolDic(discovery)
+	poolDic[Register] = newAddressPoolDic(register)
+	util.Info("初始化GrpcConnManager成功")
 	return &GrpcConnManager{poolDic: poolDic}
 }
 
-func (m *GrpcConnManager) GetConn(address string) (pool.Conn, error) {
-	p, ok := m.poolDic[address]
+// GetServiceConn 指定服务的链接
+func (m *GrpcConnManager) GetServiceConn(service ServiceType) (pool.Conn, error) {
+	if service >= ServiceTypeCount {
+		return nil, errors.New("指定的服务不存在")
+	}
+	servicesDic := m.poolDic[service]
+	p := util.RandomDicValue(servicesDic)
+	return p.Get()
+}
+
+// GetConn 指定地址的链接
+func (m *GrpcConnManager) GetConn(service ServiceType, address string) (pool.Conn, error) {
+	p, ok := m.poolDic[service][address]
 	if !ok {
 		newPool, err := pool.New(address, DefaultOptions)
 		if err != nil {
 			return nil, err
 		}
-		sdk.Info("创建连接池 %s", address)
-		m.poolDic[address] = newPool
+		util.Info("创建连接池 %s", address)
+		m.poolDic[service][address] = newPool
 		p = newPool
 	}
 	conn, err := p.Get()
