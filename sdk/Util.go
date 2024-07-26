aec737e9a0dee7c57310eca81c77838a9440c750
dongzesu@tencent.com <riyuejiuzhao@qq.com>
2024-07-26 16:02:57 +0800

    更新通用函数

--- sdk/Util.go
+++ sdk/Util.go
@@ -1,1 +1,20 @@
 package sdk
+
+import (
+	"fmt"
+	"log/slog"
+	"math/rand"
+)
+
+func RandomItem[T any](items []T) T {
+	index := rand.Intn(len(items))
+	return items[index]
+}
+
+func Info(format string, a ...any) {
+	slog.Info(fmt.Sprintf(format, a...))
+}
+
+func Error(format string, a ...any) {
+	slog.Error(fmt.Sprintf(format, a...))
+}
