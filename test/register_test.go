5b4f0f5a75fe14846ce74f75a395123d232b257f
dongzesu@tencent.com <riyuejiuzhao@qq.com>
2024-07-29 10:39:01 +0800

    添加discovery测试

--- test/register_test.go
+++ test/register_test.go
@@ -4,6 +4,7 @@ import (
 	"Supernove2024/miniRouterProto"
 	"Supernove2024/sdk"
 	"Supernove2024/sdk/config"
+	"Supernove2024/svr/discovery"
 	"Supernove2024/svr/register"
 	"Supernove2024/svr/svrutil"
 	"fmt"
@@ -28,19 +29,115 @@ func RandomPort() int32 {
 	return rand.Int31n(65536)
 }
 
-func TestRegisterSvr(t *testing.T) {
-	serviceName := "testService1"
+func RandomRegisterArgv(serviceName string, instanceNum int) map[string]*sdk.RegisterArgv {
+	testData := make(map[string]*sdk.RegisterArgv)
+	for i := 0; i < instanceNum; i++ {
+		host := RandomIP()
+		port := RandomPort()
+		address := fmt.Sprintf("%s:%v", host, port)
+		_, ok := testData[address]
+		if ok {
+			continue
+		}
+		testData[address] = &sdk.RegisterArgv{
+			ServiceName: serviceName,
+			Host:        host,
+			Port:        port,
+			Weight:      rand.Int31n(100) + 1, //保证权重不是0
+		}
+	}
+	return testData
+}
+
+func TestDiscoverSvr(t *testing.T) {
+	serviceName := "testDiscovery"
 	instanceNum := 10
 
-	testData := make([]*sdk.RegisterArgv, 0)
-	for i := 0; i < instanceNum; i++ {
-		testData = append(testData, &sdk.RegisterArgv{
-			ServiceName: serviceName,
-			Host:        RandomIP(),
-			Port:        RandomPort(),
-			Weight:      rand.Int31n(100) + 1, //保证权重不是0
-		})
+	testData := RandomRegisterArgv(serviceName, instanceNum)
+	//连接数据库
+	rdb := redis.NewClient(&redis.Options{Addr: "9.134.93.168:6380", Password: "SDZsdz2000", DB: 0})
+	//清空
+	err := rdb.FlushDB().Err()
+	if err != nil {
+		t.Fatal(err)
 	}
+
+	go register.SetupServer("127.0.0.1:8080", "9.134.93.168:6380", "SDZsdz2000", 0)
+	go discovery.SetupServer("127.0.0.1:9090", "9.134.93.168:6380", "SDZsdz2000", 0)
+
+	//等待服务器启动
+	time.Sleep(10 * time.Second)
+
+	config.DefaultConfigFilePath = "register_test.yaml"
+	registerAPI, err := sdk.NewRegisterAPI()
+	if err != nil {
+		t.Fatal(err)
+	}
+	discoveryAPI, err := sdk.NewDiscoveryAPI()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resultData := make(map[string]*sdk.RegisterArgv)
+	for _, v := range testData {
+		result, err := registerAPI.Register(v)
+		if err != nil {
+			t.Fatal(err)
+		}
+		resultData[result.InstanceID] = v
+	}
+
+	disRt, err := discoveryAPI.GetInstances(serviceName)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	disDic := make(map[string]*miniRouterProto.InstanceInfo)
+	for _, v := range disRt.Instances {
+		addrV := fmt.Sprintf("%s:%v", v.Host, v.Port)
+		disDic[addrV] = v
+	}
+	//确定双方不匹配的地方
+	for addr, v := range testData {
+		disV, ok := disDic[addr]
+		if !ok {
+			t.Errorf("discovery中缺少 Address:%s", addr)
+		}
+		if disV.Weight != v.Weight ||
+			disV.Host != v.Host ||
+			disV.Port != v.Port {
+			t.Errorf("数据不匹配，发送数据为host:%s, port:%v, weight:%v, 发现数据为host:%s, port:%v, weight:%v",
+				v.Host, v.Port, v.Weight,
+				disV.Host, disV.Port, disV.Weight)
+		}
+	}
+
+	// 上面已经确定testData <= disRt
+	// 如果相同那么就一定一样
+	if len(disRt.Instances) == len(testData) {
+		return
+	}
+
+	for addr, v := range disDic {
+		testV, ok := testData[addr]
+		if !ok {
+			t.Errorf("discovery中多出了Address:%s", addr)
+		}
+		if testV.Weight != v.Weight ||
+			testV.Host != v.Host ||
+			testV.Port != v.Port {
+			t.Errorf("数据不匹配，发送数据为host:%s, port:%v, weight:%v, 发现数据为host:%s, port:%v, weight:%v",
+				testV.Host, testV.Port, testV.Weight,
+				v.Host, v.Port, v.Weight)
+		}
+	}
+}
+
+func TestRegisterSvr(t *testing.T) {
+	serviceName := "testRegister"
+	instanceNum := 10
+
+	testData := RandomRegisterArgv(serviceName, instanceNum)
 	//链接并清空数据库
 	rdb := redis.NewClient(&redis.Options{Addr: "9.134.93.168:6380", Password: "SDZsdz2000", DB: 0})
 	err := rdb.FlushDB().Err()
