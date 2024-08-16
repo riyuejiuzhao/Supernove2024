86b320d6abf7bdeb7c691cf40b124f23c59a2606
dongzesu@tencent.com <riyuejiuzhao@qq.com>
2024-08-16 13:52:18 +0800

    添加检测的功能

--- svr/ConfigSvr.go
+++ svr/ConfigSvr.go
@@ -4,10 +4,12 @@ import (
 	"Supernove2024/pb"
 	"Supernove2024/util"
 	"context"
+	clientv3 "go.etcd.io/etcd/client/v3"
 	"google.golang.org/grpc"
 	"gopkg.in/yaml.v3"
 	"net"
 	"os"
+	"time"
 )
 
 type ClusterConfig struct {
@@ -65,11 +67,36 @@ func (s *ConfigSvr) Init(context.Context, *pb.InitArgv) (*pb.InitResult, error) 
 	}, nil
 }
 
+func checkEtcd(clusters []*ClusterConfig) {
+	for _, cluster := range clusters {
+		address := util.SliceMap(cluster.Pod, func(t *PodConfig) string {
+			return util.Address(t.Host, t.Port)
+		})
+		client, err := clientv3.New(clientv3.Config{
+			Endpoints:   address,
+			DialTimeout: 5 * time.Second,
+		})
+		if err != nil {
+			util.Error("check etcd %s err: %v", cluster.Name, err)
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+		_, err = client.Get(ctx, "Valid")
+		if err != nil {
+			util.Error("check etcd %s err: %v", cluster.Name, err)
+		}
+		cancel()
+	}
+}
+
 func (s *ConfigSvr) Serve(address string) error {
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		return err
 	}
+
+	//检测配置中的etcd是否都能相应
+	checkEtcd(s.etcdConfig.InstancesEtcd)
+	checkEtcd(s.etcdConfig.RouterEtcd)
 
 	grpcSrv := grpc.NewServer()
 	pb.RegisterConfigServiceServer(grpcSrv, s)
