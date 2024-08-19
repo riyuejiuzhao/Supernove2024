85d90319448214ad4d1ef954d137e145f6f9efc4
dongzesu@tencent.com <riyuejiuzhao@qq.com>
2024-08-20 07:05:03 +0800

    更新熔断器

--- test/test-grpc/EchoSvr_test.go
+++ test/test-grpc/EchoSvr_test.go
@@ -2,31 +2,76 @@ package test_grpc
 
 import (
 	grpc_sdk "Supernove2024/grpc-sdk"
+	"Supernove2024/sdk/metrics"
 	"Supernove2024/svr"
 	"Supernove2024/util"
 	"context"
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"log"
 	"net"
-	"sync"
 	"testing"
 	"time"
 )
 
 type EchoSvr struct {
-	Key string
+	Key    string
+	status string
 	*UnimplementedEchoServiceServer
 }
 
 func (s *EchoSvr) Echo(c context.Context, request *Request) (reply *Reply, err error) {
-	util.Info("%s recv %s", s.Key, request.Content)
-	reply = &Reply{Content: request.Content}
+	mt, err := metrics.Instance()
+	mt.InstanceRequestCount.With(prometheus.Labels{"Name": s.Key}).Inc()
+	if s.Key != "CKey" {
+		reply = &Reply{Content: request.Content}
+		util.Info("%s recv %s", s.Key, request.Content)
+	} else {
+		if s.status == "" {
+			s.status = "Sleep"
+			go func() {
+				time.Sleep(60 * time.Second)
+				s.status = "Wake"
+			}()
+			return nil, fmt.Errorf("熔断实例")
+		}
+		if s.status == "Sleep" {
+			return nil, fmt.Errorf("熔断实例")
+		}
+		reply = &Reply{Content: request.Content}
+		return
+	}
 	return
 }
 
-func EchoSetup(selfKey, otherKey, address string, opts ...grpc_sdk.ServerOption) {
+func EchoSetupClient() {
+	conn, err := grpc_sdk.NewClient("EchoService",
+		grpc_sdk.WithGrpcDialOption(
+			grpc.WithTransportCredentials(insecure.NewCredentials()),
+		))
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	client := NewEchoServiceClient(conn)
+	count := 0
+	for {
+		ctx, _ := context.WithTimeout(context.Background(), 1000*time.Second)
+		md := grpc_sdk.NewMetaData()
+		md.SetRouterType(grpc_sdk.RandomRouterType)
+		ctx = md.NewOutgoingContext(ctx)
+		_, err = client.Echo(ctx, &Request{Content: fmt.Sprintf("%v", count)})
+		if err != nil {
+			util.Error("%v", err)
+		} else {
+			count += 1
+		}
+	}
+}
+
+func EchoSetup(selfKey, address string, opts ...grpc_sdk.ServerOption) {
 	lis, err := net.Listen("tcp", address)
 	if err != nil {
 		log.Fatalln(err)
@@ -42,35 +87,6 @@ func EchoSetup(selfKey, otherKey, address string, opts ...grpc_sdk.ServerOption)
 		}
 	}()
 
-	conn, err := grpc_sdk.NewClient("EchoService",
-		grpc_sdk.WithGrpcDialOption(
-			grpc.WithTransportCredentials(insecure.NewCredentials()),
-		))
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	client := NewEchoServiceClient(conn)
-	count := 0
-	for {
-		util.Info("%s send %s-%v", selfKey, selfKey, count)
-		ctx, _ := context.WithTimeout(context.Background(), 1000*time.Second)
-		md := grpc_sdk.NewMetaData()
-		md.SetRouterType(grpc_sdk.KVRouterType)
-		err = md.AddRouterTag("Key", otherKey)
-		if err != nil {
-			util.Error("add router key err %v", err)
-			continue
-		}
-		ctx = md.NewOutgoingContext(ctx)
-		_, err = client.Echo(ctx, &Request{Content: fmt.Sprintf("%s-%v", selfKey, count)})
-		if err != nil {
-			util.Error("%v", err)
-		} else {
-			count += 1
-		}
-		time.Sleep(1 * time.Second)
-	}
 }
 
 func TestGrpc(t *testing.T) {
@@ -94,53 +110,19 @@ func TestGrpc(t *testing.T) {
 		util.ClearEtcd(addr, t)
 	}
 
-	var wg sync.WaitGroup
-	wg.Add(1)
 	go func() {
-		defer wg.Done()
-		EchoSetup("AKey", "BKey", "127.0.0.1:20000",
-			grpc_sdk.WithInstanceName("AKey"),
-			grpc_sdk.WithRouterTables([]string{"Key"}),
-			grpc_sdk.WithInboundRouters([]grpc_sdk.InboundRouterOption{
-				{
-					NextRouterType: util.WeightedRouterType,
-					Tags:           map[string]string{"Key": "AKey"},
-					InstancesName:  []string{"AKey"},
-				},
-			}))
+		EchoSetup("AKey", "127.0.0.1:20000",
+			grpc_sdk.WithInstanceName("AKey"))
 	}()
-	wg.Add(1)
 	go func() {
-		defer wg.Done()
-		EchoSetup("BKey", "CKey", "127.0.0.1:20001",
-			grpc_sdk.WithInstanceName("BKey"),
-			grpc_sdk.WithRouterTables([]string{"Key"}),
-			grpc_sdk.WithInboundRouters([]grpc_sdk.InboundRouterOption{
-				{
-					NextRouterType: util.WeightedRouterType,
-					Tags:           map[string]string{"Key": "BKey"},
-					InstancesName:  []string{"BKey"},
-				},
-			}),
-		)
+		EchoSetup("BKey", "127.0.0.1:20001",
+			grpc_sdk.WithInstanceName("BKey"))
 	}()
-	wg.Add(1)
 	go func() {
-		defer wg.Done()
-		EchoSetup("CKey", "AKey", "127.0.0.1:20002",
-			grpc_sdk.WithInstanceName("CKey"),
-			grpc_sdk.WithRouterTables([]string{"Key"}),
-			grpc_sdk.WithInboundRouters([]grpc_sdk.InboundRouterOption{
-				{
-					NextRouterType: util.WeightedRouterType,
-					Tags:           map[string]string{"Key": "CKey"},
-					InstancesName:  []string{"CKey"},
-				},
-			}),
-		)
+		EchoSetup("CKey", "127.0.0.1:20002",
+			grpc_sdk.WithInstanceName("CKey"))
 	}()
-	wg.Wait()
 
 	time.Sleep(1 * time.Second)
-	//宕机测试
+	EchoSetupClient()
 }
